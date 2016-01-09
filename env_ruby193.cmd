b9391155af3d15730a1d2c6cf79dbffd85dc3d82
Karthik Panchap <kart@codegenius.org>
2016-01-08 17:23:03 -0800

    ruby223

---
+++ env_ruby193.cmd
@@ -0,0 +1,16 @@
+@echo off
+cd /d "%~dp0"
+
+set HOME=%userprofile%
+set ROOT=%cd%
+echo ROOT is: '%root%'
+echo .
+
+set path=%windir%;%windir%\system32;%root%;%root%\bin
+
+rem we want git on our path
+set path=%path%;c:\karthik\media\apps\git\cmd
+
+rem we want console tools on our path
+set path=%path%;%tools%
+
---
+++ start_ruby193.cmd
@@ -0,0 +1,2 @@
+@echo off
+cmd.exe /k "env_ruby193.cmd"
