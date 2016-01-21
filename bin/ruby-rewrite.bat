2a69a77ef3b309d4f55473ed4c72165796194623
Karthik Panchap <kart@codegenius.org>
2016-01-20 17:29:14 -0800

    gem install rubocop --no-ri --no-rdoc

---
+++ bin/ruby-rewrite.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"c:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "c:/Users/karthik/GitHub/ruby223/bin/ruby-rewrite" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"c:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "%~dpn0" %*
