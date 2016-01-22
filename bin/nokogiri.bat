eebf43845d4c23d512fdba1b7aba0f4fc4e55fc1
Karthik Panchap <kart@codegenius.org>
2016-01-21 22:57:26 -0800

    gem update --no-ri --no-rdoc

--- bin/nokogiri.bat
+++ bin/nokogiri.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "C:/Users/karthik/GitHub/ruby223/bin/nokogiri" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"c:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "c:/Users/karthik/GitHub/ruby223/bin/nokogiri" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
-@"C:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "%~dpn0" %*
+@"c:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "%~dpn0" %*
--- bin/rake.bat
+++ bin/rake.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-ECHO.This version of Ruby has not been built with support for Windows 95/98/Me.
+@"c:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "c:/Users/karthik/GitHub/ruby223/bin/rake" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
-@"%~dp0ruby.exe" "%~dpn0" %*
+@"c:\Users\karthik\GitHub\ruby223\bin\ruby.exe" "%~dpn0" %*
